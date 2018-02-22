ac2c27f77906fa45ad08412bdcadae3dac0ab930
GeraldIr <gerald.irsiegler@gmail.com>
2018-02-22 15:37:12 +0100

    Movement is now different
    d

--- Goodbye World/Assets/Scripts/WordSpaceSpace.cs
+++ Goodbye World/Assets/Scripts/WordSpaceSpace.cs
@@ -17,25 +17,25 @@ public class WordSpaceSpace : WordSpace {
             switch(slottedWord.GetComponent<Word>().word)
             {
                 case "jump":
-                    m_player.GetComponent<CharacterMotor>().jumping.enabled = true;
+                    m_player.GetComponent<SpaceBarAction>().isJumpEnabled = true;
                     m_player.GetComponent<SpaceBarAction>().isDuckEnabled = false;
                     m_player.GetComponent<SpaceBarAction>().isFallEnabled = false;
                     break;
                 case "duck":
-                    m_player.GetComponent<CharacterMotor>().jumping.enabled = false;
+                    m_player.GetComponent<SpaceBarAction>().isJumpEnabled = false;
                     m_player.GetComponent<SpaceBarAction>().isDuckEnabled = true;
                     m_player.GetComponent<SpaceBarAction>().isFallEnabled = false;
                     break;
                 case "fall":
                     m_player.GetComponent<SpaceBarAction>().isFallEnabled = true;
                     m_player.GetComponent<SpaceBarAction>().isDuckEnabled = false;
-                    m_player.GetComponent<CharacterMotor>().jumping.enabled = false;
+                    m_player.GetComponent<SpaceBarAction>().isJumpEnabled = false;
                     break;
             }
         } else
         {
-            m_player.GetComponent<CharacterMotor>().jumping.enabled = false;
-            m_player.GetComponent<SpaceBarAction>().isDuckEnabled = false;
+            m_player.GetComponent<SpaceBarAction>().isJumpEnabled = false;
+            m_player.GetComponent<SpaceBarAction>().isFallEnabled = false;
             m_player.GetComponent<SpaceBarAction>().isDuckEnabled = false;
 
         }
