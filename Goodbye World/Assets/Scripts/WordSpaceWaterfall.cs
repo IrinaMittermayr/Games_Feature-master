2f48db055f7add78dfb81ab48d72b799c7ad6b72
GeraldIr <gerald.irsiegler@gmail.com>
2018-02-23 20:04:00 +0100

    waterfall and typos

--- Goodbye World/Assets/Scripts/WordSpaceWaterfall.cs
+++ Goodbye World/Assets/Scripts/WordSpaceWaterfall.cs
@@ -2,31 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WordSpaceWaterfall : WordSpace {
-    public GameObject bridgeAnchor;
+public class WordSpaceWaterfall : WordSpace
+{
+    public GameObject particleAnchor;
+    public Material broken;
+    public Material duck;
+    public Material water;
+    public Material glass;
+    public Material fortyfive;
+    public Material zero;
+    public Material one;
+    public Material fifteen;
+    public Material wood;
+    public Material stone;
+
 
     public override void Start()
     {
         base.Start();
 
     }
-    void Update () {
+    void Update()
+    {
         if (slottedWord != null)
         {
             switch (slottedWord.GetComponent<Word>().word)
             {
+                case "water":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = water;
+                    break;
+                case "duck":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = duck;
+                    break;
+                case "glass":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = glass;
+                    break;
+                case "wood":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = wood;
+                    break;
+                case "stone":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = stone;
+                    break;
                 case "fortyfive":
-                    //make bridge 0°
-                    bridgeAnchor.transform.rotation = Quaternion.Euler(90f, 20f, 0f);
-                break;
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = fortyfive;
+                    break;
+                case "fifteen":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = fifteen;
+                    break;
                 case "zero":
-                    //make bridge 45°
-                    bridgeAnchor.transform.rotation = Quaternion.Euler(45f, 20f, 0f);
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = zero;
+                    break;
+                case "one":
+                    particleAnchor.GetComponent<ParticleSystemRenderer>().material = one;
                     break;
             }
-        } else
-        {
-            //make bridge broken
         }
-	}
+        else
+        {
+            particleAnchor.GetComponent<ParticleSystemRenderer>().material = broken;
+        }
+    }
 }
