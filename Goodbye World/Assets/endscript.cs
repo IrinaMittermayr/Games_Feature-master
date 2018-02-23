be62d2bf27309066bcdb963db6bc0bfe156bd0d3
IrinaMittermayr <Irina.mittermayr@gmail.com>
2018-02-23 21:54:39 +0100

    Added end scene

---
+++ Goodbye World/Assets/endscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class endscript : MonoBehaviour {
+
+
+	void onTriggerEnter(){
+		onInitnScene ();
+	}
+
+	// Use this for initialization
+	public void onInitnScene () {
+		Application.LoadLevel(1);
+	}
+}
