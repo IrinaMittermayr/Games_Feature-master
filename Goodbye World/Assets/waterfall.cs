e2e15ec8b877edaa34e5ddfa1a33bcc85aa86ba1
Wertpol <robert.brock@gmx.at>
2018-02-23 22:23:52 +0100

    zweite insel, wasserfall und schild collider

---
+++ Goodbye World/Assets/waterfall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfall : MonoBehaviour {
+    public float scrollSpeed = 0.8f;
+	void Update () {
+        float offset = Time.time * scrollSpeed;
+        GetComponent<Renderer>().materials[1].SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
