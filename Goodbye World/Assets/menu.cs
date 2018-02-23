8c4b5bdfd7ae22f44a255ac8ea75ac6ec0368bb8
IrinaMittermayr <Irina.mittermayr@gmail.com>
2018-02-23 18:37:52 +0100

    Hints and menu screen
    Hints and menu screen

---
+++ Goodbye World/Assets/menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void onInitnScene (int scene) {
+		Application.LoadLevel(scene);
+	}
+}
